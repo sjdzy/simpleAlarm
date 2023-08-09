4a438d4b28d1b8331752b4221aafec8cbd809983
tmp <tmp@tmp>
2023-08-09 19:53:26 +0800

    nothingchanged

--- app/src/main/java/com/example/helloandroid/helpers/myalarmhelper.java
+++ app/src/main/java/com/example/helloandroid/helpers/myalarmhelper.java
@@ -142,6 +142,7 @@ return false;
     }
     public static String get_duration(int hour,int minute,boolean isrepeated,boolean reaptedDays[])
     {
+        //dd
         Calendar currentTime = Calendar.getInstance();
         int currentDayOfWeek = currentTime.get(Calendar.DAY_OF_WEEK);
         // 创建一个Calendar对象，用于设置闹钟的时间
